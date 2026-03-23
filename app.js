8c4ccae45185eaeb6c2b75d39c59b94d202eb906
ErkoMd <105724186+ErkoMd@users.noreply.github.com>
2026-03-23 17:08:13 -0300

    1.6

--- app.js
+++ app.js
@@ -9,17 +9,101 @@ const BASE_URL = "https://api.pubg.com/shards/";
 
 const mapMeta = {
   erangel_main: { name: "Erangel", img: "Erangel.png" },
-  baltic_main: { name: "Erangel (Remastered)", img: "Erangel.png" }, // Mapping from user
+  baltic_main: { name: "Erangel (Remastered)", img: "Erangel.png" },
   desert_main: { name: "Miramar", img: "Miramar.png" },
   savage_main: { name: "Sanhok", img: "Sanhok.png" },
   dihorotok_main: { name: "Vikendi", img: "Vikendi.png" },
-  summerland_main: { name: "Karakin", img: "Karakin.jpg" }, // Note: .jpg extension as in repo
+  summerland_main: { name: "Karakin", img: "Karakin.jpg" },
   chimera_main: { name: "Paramo", img: "Paramo.png" },
   range_main: { name: "Camp Jackal", img: "Camp_Jackal.png" },
   kiki_main: { name: "Deston", img: "Deston.png" },
   tiger_main: { name: "Taego", img: "Taego.png" },
   neon_main: { name: "Rondo", img: "Rondo.png" },
   heaven_main: { name: "Haven", img: "Haven.png" }
+};
+
+const ID_NAMES = {
+  "AIPawn_Base_Female_C": "Bot",
+  "AIPawn_Base_Male_C": "Bot",
+  "AirBoat_V2_C": "Hidroavião",
+  "AquaRail_A_01_C": "Aquarail",
+  "BP_ATV_C": "Quadriciclo",
+  "BP_BearV2_C": "Urso",
+  "BP_BRDM_C": "BRDM-2",
+  "BP_Bicycle_C": "Bicicleta",
+  "BP_CoupeRB_C": "Coupe RB",
+  "BP_Dirtbike_C": "Moto de Trilha",
+  "BP_IncendiaryDebuff_C": "Queimadura",
+  "BP_LootTruck_C": "Caminhão de Loot",
+  "BP_Motorbike_04_C": "Motocicleta",
+  "BP_Motorglider_C": "Planador",
+  "BP_PickupTruck_A_01_C": "Picape",
+  "BP_PonyCoupe_C": "Pony Coupe",
+  "BP_Scooter_01_A_C": "Scooter",
+  "BP_Snowbike_01_C": "Moto de Neve",
+  "BP_Snowmobile_01_C": "Snowmobile",
+  "BP_Van_A_01_C": "Van",
+  "BattleRoyaleModeController_Def_C": "Zona Azul",
+  "Buff_DecreaseBreathInApnea_C": "Afogamento",
+  "Carepackage_Container_C": "Caixa de Suprimentos",
+  "Dacia_A_01_v2_C": "Dacia",
+  "Jerrycan": "Galão de Combustível",
+  "Lava": "Lava",
+  "PlayerFemale_A_C": "Jogador",
+  "PlayerMale_A_C": "Jogador",
+  "ProjC4_C": "C4",
+  "ProjGrenade_C": "Granada de Fragmentação",
+  "ProjMolotov_C": "Coquetel Molotov",
+  "ProjStickyGrenade_C": "Bomba Adesiva",
+  "RedZoneBomb_C": "Zona Vermelha",
+  "WeapACE32_C": "ACE32",
+  "WeapAK47_C": "AKM",
+  "WeapAUG_C": "AUG A3",
+  "WeapAWM_C": "AWM",
+  "WeapBerreta686_C": "S686",
+  "WeapBerylM762_C": "Beryl",
+  "WeapBizonPP19_C": "Bizon",
+  "WeapCrossbow_1_C": "Besta",
+  "WeapDP12_C": "DBS",
+  "WeapDP28_C": "DP-28",
+  "WeapDesertEagle_C": "Deagle",
+  "WeapDragunov_C": "Dragunov",
+  "WeapHK416_C": "M416",
+  "WeapJS9_C": "JS9",
+  "WeapK2_C": "K2",
+  "WeapKar98k_C": "Kar98k",
+  "WeapM16A4_C": "M16A4",
+  "WeapM1911_C": "P1911",
+  "WeapM249_C": "M249",
+  "WeapM24_C": "M24",
+  "WeapM9_C": "P92",
+  "WeapMG3_C": "MG3",
+  "WeapMP5K_C": "MP5K",
+  "WeapMP9_C": "MP9",
+  "WeapMini14_C": "Mini 14",
+  "WeapMk12_C": "Mk12",
+  "WeapMk14_C": "Mk14 EBR",
+  "WeapMk47Mutant_C": "Mk47 Mutant",
+  "WeapMosinNagant_C": "Mosin-Nagant",
+  "WeapNagantM1895_C": "R1895",
+  "WeapOriginS12_C": "O12",
+  "WeapP90_C": "P90",
+  "WeapPan_C": "Frigideira",
+  "WeapPanzerFaust100M1_C": "Panzerfaust",
+  "WeapQBU88_C": "QBU88",
+  "WeapQBZ95_C": "QBZ95",
+  "WeapRhino_C": "R45",
+  "WeapSCAR-L_C": "SCAR-L",
+  "WeapSKS_C": "SKS",
+  "WeapSaiga12_C": "S12K",
+  "WeapSawnoff_C": "Cano Cerrado",
+  "WeapThompson_C": "Tommy Gun",
+  "WeapUMP_C": "UMP45",
+  "WeapUZI_C": "Micro Uzi",
+  "WeapVSS_C": "VSS",
+  "WeapVector_C": "Vector",
+  "WeapWin94_C": "Win94",
+  "WeapWinchester_C": "S1897"
 };
 
 let chartInstance = null;
@@ -194,21 +278,21 @@ function renderWeapons(data) {
     }
 
     container.innerHTML = sorted.map(([id, count]) => {
-        const cleanName = id.replace('Item_Weapon_', '').replace('Weap', '').replace('_C', '').replace('BP_', '');
-        // FIX: Precise URL mapping for weapon icons
-        const mapping = {
-            'AUG': 'AUG', 'MP5K': 'MP5K', 'HK416': 'HK416', 'Thompson': 'Thompson',
-            'Berreta686': 'Berreta686', 'QBZ95': 'QBZ95', 'M16A4': 'M16A4', 'SCAR-L': 'SCAR-L',
-            'AKM': 'AKM', 'M24': 'M24', 'Kar98k': 'Kar98k', 'Mini14': 'Mini14', 'SKS': 'SKS'
-        };
-        const weaponId = mapping[cleanName] || cleanName;
-        const imgUrl = `https://raw.githubusercontent.com/pubg/api-assets/master/Assets/Item/Weapon/Main/Item_Weapon_${weaponId}_C.png`;
+        // Use ID_NAMES for display name, fallback to cleaned ID
+        const displayName = ID_NAMES[id] || id.replace('Item_Weapon_', '').replace('Weap', '').replace('_C', '').replace('BP_', '');
+        
+        // Use the weapon internal name part for the image URL
+        const weaponId = id.includes('Item_Weapon_') ? id : `Item_Weapon_${id.replace('Weap', '').replace('BP_', '').split('_')[0]}_C`;
+        const imgId = id.replace('Weap', 'Item_Weapon_').split('_C')[0] + '_C';
+        
+        // Precise Path: Assets/Item/Weapon/Main/Item_Weapon_NAME_C.png
+        const imgUrl = `https://raw.githubusercontent.com/pubg/api-assets/master/Assets/Item/Weapon/Main/${imgId.startsWith('Item_Weapon_') ? imgId : 'Item_Weapon_' + imgId}.png`;
         
         return `
         <div class="weapon-card">
             <img src="${imgUrl}" onerror="this.src='https://cdn-icons-png.flaticon.com/512/3222/3222718.png'">
             <div class="weapon-info">
-                <span class="name">${cleanName}</span>
+                <span class="name">${displayName}</span>
                 <span class="count">${count} Abates</span>
             </div>
         </div>
